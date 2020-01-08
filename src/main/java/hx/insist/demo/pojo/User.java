17b8fedf39b4919869e5a1c0504ab36677800c70
hanhanhanxu <1195264226@qq.com>
2020-01-08 22:33:09 +0800

    finsh

---
+++ src/main/java/hx/insist/demo/pojo/User.java
@@ -0,0 +1,10 @@
+package hx.insist.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String nickName;//昵称
+    private String avatar;//头像
+    private String openid;//用户id
+}
